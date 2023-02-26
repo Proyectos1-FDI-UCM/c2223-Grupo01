637bb8d89ebb183cf8fe27fd0a026ded1826bff4
JialeHe28 <114423476+JialeHe28@users.noreply.github.com>
2023-02-26 19:10:38 +0100

    Barra de vida
    He añadido un prototipo de la barra de vida.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,16 @@ public class GameManager : MonoBehaviour
     public static GameManager instance; // Singleton inicializado en el Awake
     public GameObject _player; // referencia al player que se puede usar desde otros Scrpts sin necesidad de String Typing (NO PONER PRIVADA)
     public float _currentTime { get; private set; }
+    public float _PlayerHealth { get; private set; }
+    public float _MaxHealth = 10f;
     #endregion
-    
+
+    public void OnPlayerHit ()
+    {
+        _PlayerHealth--;
+    }
+
+
     private void Awake()
     {
         instance = this; // esto inicializa el Singleton
@@ -19,13 +27,14 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         _currentTime = 300f;
+        _PlayerHealth = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
         _currentTime -= Time.deltaTime;
-        _currentTime =_currentTime / 1;
-        Debug.Log(_currentTime);
+        _PlayerHealth -= Time.deltaTime; // Placeholder para comprobar el funcionamiento de la barra de vida.
+       
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,6 +8,9 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text _timetext;
     float _currentTime;
+    float _health; //La cantidad de vida del jugador.
+    float _maxHealth;
+    public Slider _slider;
     
    void UpdateTimer ( float Currenttime)
     {
@@ -17,11 +20,28 @@ public class UIManager : MonoBehaviour
         _timetext.text = string.Format("{0:00}:{1:00}", minutes, seconds); //Coge el archivo de texto del timer para camiarlo.
     }
 
-    // Update is called once per frame
+    public void SetMaxHealth (float health) //La máxima cantdad de vida que tendremos
+    {
+        _slider.maxValue = health; 
+        _slider.value = health;
+    }
+
+    public void SetHealth(float health) //El valor actual de nuestra vida
+    {
+        _slider.value = health;
+    }
+    void Start()
+    {
+        _maxHealth = GameManager.instance._MaxHealth;
+        SetMaxHealth(_maxHealth);
+    }
     void Update()
     {
+        _health = GameManager.instance._PlayerHealth; //En cada frame se comprobará la vida del jugador.
         _currentTime = GameManager.instance._currentTime;
         UpdateTimer(_currentTime);
+        SetHealth(_health);
+        
        
     }
 
