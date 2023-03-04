15c1c8bb94a83ac25bacfdfb019613b72a33497b
Julián Serrano Chacón <juserr04@ucm.es>
2023-03-04 21:38:01 +0100

    cambios en la velocidad de los enemigos

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -9,10 +9,13 @@ public class EnemyMovement : MonoBehaviour
 {
     #region parameters
     [SerializeField] private float _enemySpeed = 5f;
+    [SerializeField] private float _enemydetectionSpeed = 3;
+    private float _initialSpeed;
     public float KnockbackForce;                        //Cuánta fuerza tendrá el knockback.
     public float KnockbackCounter;                      //Cooldown del knockback.
     public float KnockbackTotalTime;                    //Cuánto durará el knockback.
-    public bool KnockFromRight;                         //Determina desde que posición ha sido golpeado el enemigo (derecha/izquierda).
+    public bool KnockFromRight;
+    private bool _running;//Determina desde que posición ha sido golpeado el enemigo (derecha/izquierda).
     #endregion
 
     #region references
@@ -34,6 +37,7 @@ public class EnemyMovement : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
         _player = GameManager.instance._player;
         _myEnemyFOV = GetComponent<EnemyFOV>();
+        _initialSpeed = _enemySpeed;
     }
 
     void Update()
@@ -44,20 +48,24 @@ public class EnemyMovement : MonoBehaviour
             //Si el enemigo no ha detectado al jugador, este seguirá su patrón normal
             if (!_myEnemyFOV._detected) 
             {
-                //El Enemigo se moverá a la derecha con determinada velocidad
-                _rigidbody.velocity = (transform.right * _enemySpeed);
+                // la velocidad de patrullaje es la inicial
+                _enemySpeed = _initialSpeed;
+                _running = false;
             }
             //Si el enemigo nos detecta. 
-            else if (_myEnemyFOV._detected) 
+            else if (_myEnemyFOV._detected && !_running) 
             {
                 //la dirección a la que nuestro enemigo se moverá.
-                Vector2 Distfromplayer = new Vector2(_player.transform.position.x - transform.position.x, 0f); 
+                /* Distfromplayer = new Vector2(_player.transform.position.x - transform.position.x, 0f); 
                 Vector2 velocity = Distfromplayer * _enemySpeed;
-                _rigidbody.velocity = velocity;
+                _rigidbody.velocity = velocity;*/ //esto es lo que hizo jiale, aumenta la velocidad con la distancia al player (mal)
+                _enemySpeed += _enemydetectionSpeed;
+                _running = true;
             }
+            //El Enemigo se moverá a la derecha con determinada velocidad
+            _rigidbody.velocity = (transform.right * _enemySpeed);
         }
-        //Si está en modo knockback
-        else
+        else //Si está en modo knockback
         {
             //Si golpea por la derecha...
             if (KnockFromRight) 
