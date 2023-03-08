3f5dabd3c3b7323c1745fff0b3fdc5bce5e3ec93
TheJhosLOL <125390017+JoseAntonioCA@users.noreply.github.com>
2023-03-08 01:04:21 +0100

    arreglando todo para la demo

--- Assets/Scripts/MightyLifeComponent.cs
+++ Assets/Scripts/MightyLifeComponent.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class MightyLifeComponent : MonoBehaviour
 {
