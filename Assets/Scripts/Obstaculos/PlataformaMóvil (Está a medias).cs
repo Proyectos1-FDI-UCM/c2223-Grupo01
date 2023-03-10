6f50be60f4933aebc8a29a64b7d59ee2649a7e2c
Bienen27 <125396552+Bienen27@users.noreply.github.com>
2023-03-11 00:35:46 +0100

    Comienzo plataforma móvil
    He empezado a crear lo de la plataforma que se mueve.

---
+++ Assets/Scripts/Obstaculos/PlataformaMóvil (Está a medias).cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaMóvil : MonoBehaviour
+{
+    #region References
+    [SerializeField] GameObject _objetoamover;
+    [SerializeField] Transform _puntodePartida;
+    [SerializeField] Transform _puntoFinal;
+    private Vector3 _moverHacia;
+    
+        
+    
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
