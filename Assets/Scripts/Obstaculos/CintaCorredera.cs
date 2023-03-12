329af9ac3b9fab07baa9f18460706bec8df42335
JoseAntonioCA <jocarm02@ucm.es>
2023-03-12 14:45:21 +0100

    Los scripts de disparo vuelven a estar en su sitio, cintas direccionadas implementadas, y retocados algunos sprites

---
+++ Assets/Scripts/Obstaculos/CintaCorredera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CintaCorredera : MonoBehaviour
+{
+    #region Parameters
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector2 _direccion;
+    #endregion
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<MightyLifeComponent>() != null && collision.gameObject.GetComponent<CharacterController>()._isgrounded)
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(_speed * _direccion);
+        }
+    }
+}
