7bdbf9aa02798f0309b36ab680d533e9278126b1
JialeHe28 <114423476+JialeHe28@users.noreply.github.com>
2023-03-14 23:33:34 +0100

    Cambios en el script

--- Assets/Scripts/Obstaculos/PlataformasGiratorias.cs
+++ Assets/Scripts/Obstaculos/PlataformasGiratorias.cs
@@ -10,31 +10,23 @@ public class PlataformasGiratorias : MonoBehaviour
     [SerializeField]private float _rotationSpeed;
     [SerializeField]private float _setTime = 3;
     
-
-    private void RotatePlatform() 
+    void Update()
+    {
         //El método incluye un contador que determina cada cuanto ejecuta la rotación.
         //Cuando se ejecuta la rotatación, le sumo a la rotación actual un valor y lo redondeo.
-    {     
         _countdowntime += Time.deltaTime;
 
         if (_countdowntime > _setTime)
         {
-            
-             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0,0, _DesiredRotation), _rotationSpeed * Time.deltaTime);
-           
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, _DesiredRotation), _rotationSpeed * Time.deltaTime);
+
             if (transform.rotation.eulerAngles.z == _DesiredRotation)
 
             {
                 _DesiredRotation = (_DesiredRotation + 90f) % 360;
                 _countdowntime = 0;
             }
-        }                    
-    }
-   
-    
-    
-    void Update()
-    {
-       RotatePlatform();    
+        }
     }
 }
