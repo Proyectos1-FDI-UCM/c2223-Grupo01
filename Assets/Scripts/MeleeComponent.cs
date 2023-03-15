b9a07cc072e9baabe79c892ecd786cf972c49bd2
Julián Serrano Chacón <juserr04@ucm.es>
2023-03-15 10:54:41 +0100

    melee eliminada una variable que sobra

--- Assets/Scripts/MeleeComponent.cs
+++ Assets/Scripts/MeleeComponent.cs
@@ -11,7 +11,6 @@ public class MeleeComponent : MonoBehaviour
     [SerializeField] private int attackDamage = 40;     //Daño por golpe.
     [SerializeField] private float attackRate = 2f;     //Indica cuántas veces se va a atacar por segundo.
     private float nextAttackTime = 0f;                  //Cooldown del ataque.
-    private EnemyMovement _enemyMovement = null;        //Sirve para comunicarse con el script de "EnemyMovement".
     [SerializeField] private float _coolDownMelee;      //tiempo en el que se permitirá usar el arma a melee
     private float _initialCoolDownMelee;
 
