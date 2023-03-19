998de70f5e675025f95522151e0e27c87bb9c4bd
Mauro Martínez <maumar03@ucm.es>
2023-03-19 22:05:39 +0100

    Curación del checkpoint semiacabada
    Solo falta solucionar el problema de la UI y barra de vida

--- Assets/Scripts/Checkpoint/CheckpointInteractionComponent.cs
+++ Assets/Scripts/Checkpoint/CheckpointInteractionComponent.cs
@@ -9,25 +9,26 @@ public class CheckpointInteractionComponent : MonoBehaviour
 {
     #region references
     private GameObject _player;
+    private MightyLifeComponent _mightyhealth;
     #endregion
 
     #region parameters
     [SerializeField] private string _playertag;
     #endregion
 
-    // Start is called before the first frame update
-    //void Start()
-    //{
-
-    //}
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag(_playertag))
         {
+            // referencias de la colisión
             _player = collision.gameObject;
+            _mightyhealth = GetComponent<MightyLifeComponent>();
 
-            Debug.Log(_player.name);
+            _mightyhealth._health = 100;
+            //if (GameManager.instance._UImanager != null)
+            //{
+            //    GameManager.instance._UImanager.ActualizarInterfaz(_mightyhealth._health);
+            //}
 
 
 
