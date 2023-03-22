d3b8f5ef0f04a9fdc0391149c8e87a15a6b62229
JialeHe28 <114423476+JialeHe28@users.noreply.github.com>
2023-03-22 10:04:36 +0100

    Las pLataformas giran con RigidBody

--- Assets/Scripts/Obstaculos/PlataformasGiratorias.cs
+++ Assets/Scripts/Obstaculos/PlataformasGiratorias.cs
@@ -7,6 +7,7 @@ public class PlataformasGiratorias : MonoBehaviour
 {
     private float _Timer = 0f;
     private float _initialrotation;
+    private Rigidbody2D _rigidbody;
     [SerializeField]private float _DesiredRotation;
     [SerializeField]private float _rotationSpeed;
     [SerializeField]private float _setTime = 3;
@@ -14,6 +15,7 @@ public class PlataformasGiratorias : MonoBehaviour
     private void Start()
     {
         _initialrotation=_DesiredRotation;
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
@@ -23,10 +25,11 @@ public class PlataformasGiratorias : MonoBehaviour
         Debug.Log(transform.rotation.eulerAngles.z + "  " + _DesiredRotation);
         _Timer += Time.deltaTime;
 
-        if (_Timer > _setTime)
-        {
 
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, _DesiredRotation), _rotationSpeed * Time.deltaTime);
+        if (_Timer > _setTime)
+            
+        {
+            _rigidbody.MoveRotation (Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, 0, _DesiredRotation), _rotationSpeed * Time.deltaTime));
 
             if (Mathf.Approximately(transform.rotation.eulerAngles.z, _DesiredRotation))
 
