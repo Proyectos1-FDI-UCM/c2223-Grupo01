8b6828454eb30022d293e531c2285035a6855fcc
JoseAntonioCA <jocarm02@ucm.es>
2023-03-27 02:40:44 +0200

    Retocados parametros de enemigos, añadido cambio de escenas provisional, y arreglados/pulidos varios aspectos estéticos y mecánicos en parte de los niveles

--- Assets/Scripts/Menu Inicial/IntroMenuCommands.cs
+++ Assets/Scripts/Menu Inicial/IntroMenuCommands.cs
@@ -9,6 +9,22 @@ public class IntroMenuCommands : MonoBehaviour
     {
         SceneManager.LoadScene(2);
     }
+    public void ComenzarHIELO()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void ComenzarFABRICA()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void ComenzarHIELOmitad()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void ComenzarFABRICAmitad()
+    {
+        SceneManager.LoadScene(6);
+    }
     public void SalirDeljuego()
     {
         Application.Quit();
