12473290c5bf24556a55f967cbd6d79c64609526
Julián Serrano Chacón <juserr04@ucm.es>
2023-04-02 01:22:49 +0200

    cambios

--- Assets/Scripts/Final Boss/FinalBossManager.cs
+++ Assets/Scripts/Final Boss/FinalBossManager.cs
@@ -4,6 +4,5 @@ using UnityEngine;
 
 public class FinalBossManager : MonoBehaviour
 {
-    //El script controlará el comportamiento general, detectará la muerte de las manos y activará la cabeza.
     
 }
