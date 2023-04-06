4dbdfce39086dd12a1960aab4f4de11caf62d0f1
Julián Serrano Chacón <juserr04@ucm.es>
2023-04-06 13:43:19 +0200

    Patroll de las manos

--- Assets/Scripts/Final Boss/HandsManager.cs
+++ Assets/Scripts/Final Boss/HandsManager.cs
@@ -9,10 +9,17 @@ public class HandsManager : MonoBehaviour
     //transforms de barrido
     [Header("Manos")]
     [SerializeField] private GameObject[] _hands;
+    [SerializeField] private LayerMask _layerManos;
+    [SerializeField] private float _turCoolDown;
+    private float _turCoolDownInicial;
+    private bool _canturn;
     [Header("Barrido")]
     [SerializeField] private Transform[] _sweepPositions; //_leftDown, _leftTop, _rightDown, _rightUp
     [Header("Caida")]
     public Transform[] _upPositions;
+    //velocidad yrotación 
+    private bool _isflipped; //Si el enemigo ha dado la vuelta
+    [SerializeField] private float _enemySpeed = 5f;
     //estados de las manos 
     private enum HandsStates {Patrullaje, Transición, Barrido }; 
     private HandsStates _currenState; //estado actual
@@ -25,10 +32,7 @@ public class HandsManager : MonoBehaviour
         {
             case HandsStates.Patrullaje:
                 {
-                    foreach(GameObject _hand in _hands)
-                    {
-                        
-                    }
+                    PatrullajeUpdate();
                     break;
                 }
             case HandsStates.Transición:
@@ -41,12 +45,48 @@ public class HandsManager : MonoBehaviour
                 }
         }
     }
+
+    private void PatrullajeUpdate()
+    {
+        foreach (GameObject _hand in _hands)
+        {
+            if (!_isflipped)
+            {
+                _hand.GetComponent<Rigidbody2D>().velocity = (Vector3.right * _enemySpeed);
+            }
+            else
+            {
+                _hand.GetComponent<Rigidbody2D>().velocity = (Vector3.left * _enemySpeed);
+            }
+            
+            if((Physics2D.BoxCast(_hand.GetComponent<Collider2D>().bounds.center, 
+                _hand.GetComponent<Collider2D>().bounds.size, 0f, Vector2.right, .001f, _layerManos) ||
+                Physics2D.BoxCast(_hand.GetComponent<Collider2D>().bounds.center,
+                _hand.GetComponent<Collider2D>().bounds.size, 0f, Vector2.left, .001f, _layerManos)) && _canturn)
+            {
+                _isflipped = !_isflipped;
+                _canturn = false;
+            }
+
+            if (!_canturn)
+            {
+                _turCoolDown -= Time.deltaTime;
+                if(_turCoolDown <= 0)
+                {
+                    _canturn = true;
+                    _turCoolDown = _turCoolDownInicial;
+                }
+            }
+        }
+    }
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
         _currenState = HandsStates.Patrullaje;
+        _canturn = true;
+        _turCoolDownInicial = _turCoolDown;
     }
 
     // Update is called once per frame
