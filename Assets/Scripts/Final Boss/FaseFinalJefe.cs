4cdda5dcf53de74bbb4d00a956c4be3954bcae88
JoseAntonioCA <jocarm02@ucm.es>
2023-04-12 19:38:33 +0200

    puto raycast

--- Assets/Scripts/Final Boss/FaseFinalJefe.cs
+++ Assets/Scripts/Final Boss/FaseFinalJefe.cs
@@ -5,25 +5,27 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class FaseFinalJefe : MonoBehaviour
 {
-    private Transform _myTransform;
+    [SerializeField] private Transform _myTransform;
+    public GameObject _player;
     private int _patata = 0;
 
     [SerializeField] private LayerMask _playerLayer;
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawRay(_myTransform.position, GameManager.instance._player.transform.position);
+        Gizmos.DrawRay(_myTransform.position, _player.transform.position);
     }
     // Start is called before the first frame update
     void Start()
     {
         _myTransform = GetComponent<Transform>();
+        _player = GameManager.instance._player;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Physics2D.Raycast(_myTransform.position, GameManager.instance._player.transform.position, 100.0f, _playerLayer))
+        if(Physics2D.Raycast(_myTransform.position, _player.transform.position, 100.0f, _playerLayer))
         {
             _patata++;
             Debug.Log("debug" + _patata);
