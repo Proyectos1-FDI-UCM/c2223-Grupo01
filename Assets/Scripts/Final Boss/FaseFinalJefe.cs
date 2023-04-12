2d298a233948c512d59a09ba3f1612a11ff338a6
JoseAntonioCA <jocarm02@ucm.es>
2023-04-12 23:05:18 +0200

    Fase 2 del jefe final terminada

--- Assets/Scripts/Final Boss/FaseFinalJefe.cs
+++ Assets/Scripts/Final Boss/FaseFinalJefe.cs
@@ -6,18 +6,39 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 public class FaseFinalJefe : MonoBehaviour
 {
     [SerializeField] private Transform _myTransform;
-    private GameObject _player;
+    [SerializeField] private GameObject _player;
+
+    [SerializeField] private GameObject _bullet; //prefab de la bala
+    [SerializeField] private float _coolDownShoot; //Contador para poder volver a disparar
+    [SerializeField] private Transform _bulletSpawnTransform;
+
+    private float _initialCoolDownShoot; //Valor inicial del contador de disparo, util para reconfigurar el cooldown que se modifica en el Update
+
+    private bool _canShoot; //Booleano que determina si puede disparar o no
+
     private int _patata = 0;
 
     [SerializeField] private LayerMask _playerLayer;
 
+    public void Shoot()
+    // instanciamos la bala en la posición del spawn (cuidado no es hija suya, no confundir con la sobrecarga del transform del parent)
+    {
+        Instantiate(_bullet, _bulletSpawnTransform.position, _bulletSpawnTransform.rotation);
+
+        Debug.Log("dispara");
+        _canShoot = false;
+    }
+
     private void OnDrawGizmos()
     {
-        Gizmos.DrawRay(_myTransform.position, _player.transform.position);
+        Gizmos.DrawRay(_myTransform.position, _player.transform.position - _myTransform.position);
     }
     // Start is called before the first frame update
     void Start()
     {
+        _initialCoolDownShoot = _coolDownShoot;
+        _canShoot = true;
+
         _myTransform = GetComponent<Transform>();
         _player = GameManager.instance._player;
     }
@@ -25,8 +46,22 @@ public class FaseFinalJefe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Physics2D.Raycast(_myTransform.position, _player.transform.position, 100.0f, _playerLayer))
+        if(Physics2D.Raycast(_myTransform.position, _player.transform.position - _myTransform.position, 100.0f, _playerLayer))
         {
+            if (_canShoot)
+            {
+                _coolDownShoot = _initialCoolDownShoot;
+                Shoot();
+            }
+            else
+            {
+                _coolDownShoot -= Time.deltaTime;
+                if (_coolDownShoot <= 0)
+                {
+                    _canShoot = true;
+                }
+            }
+
             _patata++;
             Debug.Log("debug" + _patata);
         }
