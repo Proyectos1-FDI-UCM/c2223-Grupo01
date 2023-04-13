37a6d73841abf524ef99f78070a145f12b60978e
Julián Serrano Chacón <juserr04@ucm.es>
2023-04-13 17:49:49 +0200

    optimización mejorada de las manos
    A veces giran raro

--- Assets/Scripts/Final Boss/HandsManager.cs
+++ Assets/Scripts/Final Boss/HandsManager.cs
@@ -12,13 +12,10 @@ public class HandsManager : MonoBehaviour
     [Header("Manos")]
     [SerializeField] private GameObject[] _hands;
     [SerializeField] private LayerMask _layerManos;
-    [SerializeField] private float _turCoolDown;
-    private float _turCoolDownInicial;
-    private bool _canturn;
     [Header("Barrido")]
     [SerializeField] private Transform[] _sweepPositions; //_leftDown, _leftTop, _rightDown, _rightUp
-    [Header("Caida")]
     [SerializeField] private Transform[] _upPositions;
+    [Header("Caida")]
     [SerializeField] private LayerMask _layerPlayer;
     [SerializeField] private LayerMask _layerSuelo;
     private int _tipoDeCaida;
@@ -27,7 +24,6 @@ public class HandsManager : MonoBehaviour
     private bool _caido;
     [SerializeField] private float _caidaSpeed;
     //velocidad yrotación 
-    private bool _isflipped; //Si el enemigo ha dado la vuelta
     [SerializeField] private float _enemySpeed = 5f;
     //estados de las manos 
     private enum HandsStates {Patrullaje, Transición, Barrido }; 
@@ -82,29 +78,24 @@ public class HandsManager : MonoBehaviour
     {
         foreach (GameObject _hand in _hands)
         {
+            if (_enemySpeed > 0 && Physics2D.BoxCast(_hand.GetComponent<Collider2D>().bounds.center,
+                _hand.GetComponent<Collider2D>().bounds.size, 0f, Vector2.right, .001f, _layerManos))
+            {
+                _enemySpeed *= -1;
+                PatrullajeMovement();
+            }
+            else if (_enemySpeed < 0 && Physics2D.BoxCast(_hand.GetComponent<Collider2D>().bounds.center,
+                    _hand.GetComponent<Collider2D>().bounds.size, 0f, Vector2.left, .0005f, _layerManos))
+            {
+                _enemySpeed *= -1;
+                PatrullajeMovement();
+            }
             if (!_caido)
             {
                 PatrullajeMovement();
             }
-            DetectordeCaida();
-            if((Physics2D.BoxCast(_hand.GetComponent<Collider2D>().bounds.center, 
-                _hand.GetComponent<Collider2D>().bounds.size, 0f, Vector2.right, .001f, _layerManos) ||
-                Physics2D.BoxCast(_hand.GetComponent<Collider2D>().bounds.center,
-                _hand.GetComponent<Collider2D>().bounds.size, 0f, Vector2.left, .001f, _layerManos)) && _canturn)
-            {
-                _isflipped = !_isflipped;
-                _canturn = false;
-            }
 
-            if (!_canturn)
-            {
-                _turCoolDown -= Time.deltaTime;
-                if(_turCoolDown <= 0)
-                {
-                    _canturn = true;
-                    _turCoolDown = _turCoolDownInicial;
-                }
-            }
+            DetectordeCaida();
 
             if (_caido)
             {
@@ -117,14 +108,7 @@ public class HandsManager : MonoBehaviour
     {
         foreach (GameObject _hand in _hands)
         {
-            if (!_isflipped)
-            {
-                _hand.GetComponent<Rigidbody2D>().velocity = (Vector3.right * _enemySpeed);
-            }
-            else
-            {
-                _hand.GetComponent<Rigidbody2D>().velocity = (Vector3.left * _enemySpeed);
-            }
+            _hand.GetComponent<Rigidbody2D>().velocity = (Vector3.right * _enemySpeed);
         }
     }
     private void DetectordeCaida()
@@ -158,10 +142,7 @@ public class HandsManager : MonoBehaviour
     }
     public void ChangeCaidaSpeed()
     {
-        if (_canturn)
-        {
             _caidaSpeed *= -1;
-        }
     }
 
     private void MovimientoCaida()
@@ -212,8 +193,6 @@ public class HandsManager : MonoBehaviour
     void Start()
     {
         _currenState = HandsStates.Patrullaje;
-        _canturn = true;
-        _turCoolDownInicial = _turCoolDown;
         _tocaCaer = Random.Range(minCaida, maxCaida);
         _vecesPasado = 0;
     }
