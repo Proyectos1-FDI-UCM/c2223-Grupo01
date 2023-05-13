c51caf8d81a2ab5dadafe837aa40f7605dac97db
Jose Antonio <jocarm02@ucm.es>
2023-05-13 18:52:34 +0200

    Juego casi terminado, faltan implementar cinemáticas, voy a eso ahora mismo

--- Assets/Scripts/Menu Inicial/SkipIntroVideo.cs
+++ Assets/Scripts/Menu Inicial/SkipIntroVideo.cs
@@ -8,7 +8,6 @@ public class SkipIntroVideo : MonoBehaviour
     private float _timer = 0.0f;
     [SerializeField] private float _maxTimer;
     [SerializeField] private int _sceneIndex;
-    private UniversalInput _newInput;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +19,7 @@ public class SkipIntroVideo : MonoBehaviour
     void Update()
     {
         _timer += Time.deltaTime;
-        if (_timer > _maxTimer || _newInput.Mighty.Jump.triggered)
+        if (_timer > _maxTimer)
         {
             SceneManager.LoadScene(_sceneIndex);
         }
